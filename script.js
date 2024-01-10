fb72f8fb8006c51f2d93f3b22b8e43bdeb3ef356
trangdangberlin <dang38073@gmail.com>
2024-01-10 21:05:13 +0100

    change button for color randomly

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 // script.js
 function changeColor() {
-  const newColor = '#0f9d58';
-  document.body.style.backgroundColor = newColor;
+  // Generate random RGB values
+  const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  document.body.style.backgroundColor = randomColor;
 }
