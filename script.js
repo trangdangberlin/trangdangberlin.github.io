7dcbc6b6589f7d16ea63032b9220cc92c0c5179f
trangdangberlin <dang38073@gmail.com>
2024-01-10 21:09:53 +0100

    Add console log

--- script.js
+++ script.js
@@ -3,5 +3,6 @@
 function changeColor() {
   // Generate random RGB values
   const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  console.log(randomColor)
   document.body.style.backgroundColor = randomColor;
 }
